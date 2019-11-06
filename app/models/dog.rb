0387221fd59b6c86fc2dcae450deb7dda47064e5
Brandon Brasson <rocksong622@gmail.com>
2019-11-06 16:04:27 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,17 +1,17 @@
 class Dog
-attr_accessor :name, :breed, :age
- DOGS = []
+  attr_accessor :name, :breed, :age
+  DOGS = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    DOGS << self
+  end
 
-def initalize(name, breed, age)
-  @name = name
-  @breed = breed
-  @age = age
- DOGS << self
-end
-
-def self.all
-DOGS
-  #@all
-end
+  def self.all
+    DOGS
+  end
 
 end
+
+© 2019 GitHub
